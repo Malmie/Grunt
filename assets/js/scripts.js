f5779079cf7f7f01c3cb2db0c291ab9be20ac8fc
Malmie <miechurska.m88@gmail.com>
2017-10-29 08:05:08 +0100

    js file path

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,13 @@
 // scripts.js
 
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+//this is one line comment to the below element
+alert('Hello, ' + name); 
 console.log('Hello, ' + name);
+
+/*
+this is block comment:
+var name = prompt('Enter your name');
+alert('Hello, ' + name); 
+console.log('Hello, ' + name);
+*/
